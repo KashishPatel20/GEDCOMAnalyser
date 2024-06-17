92423b7747c54cf73db5ed5beb17eeec776e1f23
babelman20 <bradley@qfmedical.com>
2024-06-16 22:43:16 -0400

    Fixed family organization to fit new GEDCOM format

--- CS555Proj.py
+++ CS555Proj.py
@@ -1,208 +1,163 @@
 from pathlib import Path
 from prettytable import PrettyTable
-from datetime import date
+from datetime import datetime
 
-def dateCalc(date):
-    med = ""
-    if(not ((date[1]).isnumeric())):
-        date = "0" + date
-    if(date[2:5] == "JAN"):
-        med = "01"
-    elif(date[2:5] == "FEB"):
-        med = "02"
-    elif(date[2:5] == "MAR"):
-        med = "03"
-    elif(date[2:5] == "APR"):
-        med = "04"
-    elif(date[2:5] == "MAY"):
-        med = "05"
-    elif(date[2:5] == "JUN"):
-        med = "06"
-    elif(date[2:5] == "JUL"):
-        med = "07"
-    elif(date[2:5] == "AUG"):
-        med = "08"
-    elif(date[2:5] == "SEP"):
-        med = "09"
-    elif(date[2:5] == "OCT"):
-        med = "10"
-    elif(date[2:5] == "NOV"):
-        med = "11"
-    elif(date[2:5] == "DEC"):
-        med = "12"
-    else:
-        #some error
-        pass
-    date = date[5:] + "-" + med + "-" + date[0:2]
-    return date
+class Individual:
+    def __init__(self, id: str):
+        self.IId: str = id
+        self.IName: str = "NA"
+        self.IGen: str = "NA"
+        self.IBirth: datetime = None
+        self.IDeath: datetime = "NA"
+        self.IChild: str = "NA"
+        self.ISpouse: str = "NA"
 
+    def is_alive(self) -> bool:
+        return (self.IDeath is not None)
+    
+    def get_age(self) -> int:
+        if self.is_alive(): enddate = datetime.now()
+        else: enddate = self.IDeath
+
+        age = enddate.year - self.IBirth.year
+        if enddate.month < self.IBirth.month: age -= 1  # Birth month had not yet come
+        elif enddate.month == self.IBirth.month and enddate.day < self.IBirth.day: age -= 1  # Bith day has not yet come
+
+        return age
+
+class Family:
+    def __init__(self, id: str):
+        self.FId: str = id
+        self.FMar: datetime = "NA"
+        self.FDiv: datetime = "NA"
+        self.FHusbId: str = "NA"
+        self.FWifeId: str = "NA"
+        self.FChildIds = []
+
+
+HANDLED_TAGS = ['INDI', 'NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'FAM', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV', 'DATE']
+SPECIAL_TAGS = ['INDI', 'FAM']
+
+# Parse out the tag and content from each line
+def parse_line(line: str):
+    split_content = line.split()
+
+    if len(split_content) >= 3 and split_content[2] in SPECIAL_TAGS:  # Special line for INDI or FAM
+        tag = split_content[2]
+        content = split_content[1]
+    elif len(split_content) >= 2 and split_content[1] in HANDLED_TAGS:  # Regular accepted line tag
+        tag = split_content[1]
+        content = ' '.join(split_content[2:])
+    else: return None  # Line tag is not accepted
+
+    return (tag, content)
+
+
+individuals = {}
+families = {}
+
+# Parse data from GEDCOM file
+path = Path(__file__).with_name('babelman_GEDCOM.ged')
+with path.open('r') as file:
+    individual = None
+    family = None
+
+    lines = file.readlines()
+    index = 0
+    while index < len(lines):
+        line = lines[index].rstrip()
         
-p = Path(__file__).with_name('GEDCOM_DATA.txt')
-with p.open('r') as f:
-    data = f.read()
-    newdata = data.splitlines()
-    count = 0
-    newcount = 1
-    indivTable = PrettyTable()
-    indivTable.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"]
-    IId, IName, IGen, IBirth, IAge, IAlive, IDeath, IChild, ISpouse = "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA"
-    famTable = PrettyTable()
-    famTable.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
-    FId, FMar, FDiv, FHId, FHName, FWId, FWName, FChild = "NA", "NA", "NA", "NA", "NA", "NA", "NA", []
-    dateCheck = "NA" #can be BIRT DEAT DIV or MARR
-    typeCheck = "" #can be INDI or FAM
-    firstPassInd, firstPassFam = 0, 0
-    husbName, wifeName = "NA", "NA"
-    for e in newdata:
-        if(e.strip() == ''):
-            count+=1
-        else:
-            line = newdata[count]
-            lineList = line.split(' ')
-            count+=1
-            level = ''
-            tag = ''
-            args = ''
-            for ele in lineList:
-                if(newcount == 1):
-                    level = ele
-                elif(newcount == 2):
-                    tag = ele
-                else:
-                    args += ele
-                newcount+=1
-            newcount = 1
-            valid = 'Y'
-            if(tag == 'INDI' or tag == 'FAM' or tag == 'HEAD' or tag == 'TRLR' or tag == 'NOTE'):
-                #0th level cases
-                if(args == 'INDI'):
-                    IId = args
-                elif(args == 'FAM'):
-                    #FAM
-                    FId = args
-                pass
-            elif(tag == 'NAME' or tag == 'SEX' or tag == 'BIRT' or tag == 'DEAT' or tag == 'FAMC' or tag == 'FAMS' or tag == 'MARR' or tag == 'HUSB' or tag == 'WIFE' or tag == 'CHIL' or tag == 'DIV'):
-                #1st level cases
-                if(tag == 'NAME'):
-                    IName = args
-                elif(tag == 'SEX'):
-                    IGend = args
-                elif(tag == 'BIRT'):
-                    IBirth = args
-                    dateCheck = 'BIRT'
-                elif(tag == "DEAT"):
-                    IDeath = args
-                    dateCheck = 'DEAT'
-                elif(tag == 'FAMC'):
-                    IChild = args
-                    ISpouse = "NA"
-                elif(tag == 'FAMS'):
-                    IChild = "NA"
-                    ISpouse = args
-                    if(IGend == "M"):
-                        husbName = IName
-                    elif(IGend == "F"):
-                        wifeName = IName
-                    else:
-                        pass
-                elif(tag == 'MARR'):
-                    FMar = args
-                    FDiv = "NA"
-                    dateCheck = 'MARR'
-                elif(tag == 'HUSB'):
-                    FHId = args
-                elif(tag == 'WIFE'):
-                    FWId = args
-                elif(tag == 'CHIL'):
-                    FChild.append(args)
-                else:
-                    #tag == Div
-                    FMar = "NA"
-                    FDiv = args
-                    dateCheck = 'DIV'
-                pass 
-            elif(tag == 'DATE'):
-                #2nd level case
-                if(dateCheck == 'BIRT'):
-                    IBirth = args
-                elif(dateCheck == 'DEAT'):
-                    IDeath = args
-                elif(dateCheck == 'MARR'):
-                    FMar = args
-                else:
-                    #dateCheck == 'DIV'
-                    FDiv = args
-                pass 
-            else:
-                if(args == 'INDI' or args == 'FAM'):
-                    #if tag is INDI or FAM, the 'tag' input wouldve been the identifier, meaning 'args' would hold INDI or FAM
-                    if(typeCheck == 'INDI' and firstPassInd >= 1):
-                        IBirth = dateCalc(IBirth)
-                        if(not (IDeath == "NA")):
-                            IDeath = dateCalc(IDeath)
-                            IAge = int(IDeath[0:4]) - int(IBirth[0:4])
-                            IAlive = False
-                        else:
-                            today = date.today()
-                            IAge = today.year - int(IBirth[0:4])
-                            IAlive = True
-                        indivTable.add_row([IId, IName, IGend, IBirth, IAge, IAlive, IDeath, IChild, ISpouse])
-                        IId, IName, IGen, IBirth, IAge, IAlive, IDeath, IChild, ISpouse = "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA"
-                        dateCheck = "NA"
-                        typeCheck = ""
-                    if(typeCheck == 'FAM' and firstPassFam >= 1):
-                        if(not (FMar == "NA")):
-                            FMar = dateCalc(FMar)
-                        if(not (FDiv == "NA")):
-                            FDiv = dateCalc(FDiv)
-                        if(len(FChild) < 1):
-                            FChild = "NA"
-                        FHName = husbName
-                        FWName = wifeName
-                        famTable.add_row([FId, FMar, FDiv, FHId, FHName, FWId, FWName, FChild])
-                        FId, FMar, FDiv, FHId, FHName, FWId, FWName, FChild = "NA", "NA", "NA", "NA", "NA", "NA", "NA", []
-                        dateCheck = "NA"  
-                        typeCheck = ""
-                        husbName, wifeName = "NA", "NA"
-                    if(args == 'INDI'):
-                        IId = tag
-                        typeCheck = args
-                        firstPassInd +=1
-                    else:
-                        #FAM
-                        FId = tag
-                        typeCheck = args
-                        firstPassFam +=1
-                    pass
-                else:
-                    valid = 'N'
-    if(typeCheck == 'INDI' and firstPassInd == 1):
-        IBirth = dateCalc(IBirth)
-        if(not (IDeath == "NA")):
-            IDeath = dateCalc(IDeath)
-            IAge = int(IDeath[0:4]) - int(IBirth[0:4])
-            IAlive = False
-        else:
-            today = date.today()
-            IAge = today.year - int(IBirth[0:4])
-            IAlive = True
-        indivTable.add_row([IId, IName, IGend, IBirth, IAge, IAlive, IDeath, IChild, ISpouse])
-        IId, IName, IGen, IBirth, IAge, IAlive, IDeath, IChild, ISpouse = "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA"
-        dateCheck = "NA"
-        typeCheck = ""
-    if(typeCheck == 'FAM' and firstPassFam == 1):
-        if(not (FMar == "NA")):
-            FMar = dateCalc(FMar)
-        if(not (FDiv == "NA")):
-            FDiv = dateCalc(FDiv)
-        if(len(FChild) < 1):
-            FChild = "NA"
-        FHName = husbName
-        FWName = wifeName
-        famTable.add_row([FId, FMar, FDiv, FHId, FHName, FWId, FWName, FChild])
-        FId, FMar, FDiv, FHId, FHName, FWId, FWName, FChild = "NA", "NA", "NA", "NA", "NA", "NA", "NA", []
-        dateCheck = "NA"  
-        typeCheck = ""
-        husbName, wifeName = "NA", "NA"
-    print(indivTable)
-    print(famTable)
+        data = parse_line(line)
+        if data is None: 
+            index += 1
+            continue
+
+        tag, content = data
+
+        # Handle each accepted tag
+        if tag == 'INDI':
+            if individual is not None: individuals[individual.IId] = individual
+            id = content[1:-1]
+            individual = Individual(id)
+        elif tag == 'FAM':
+            if family is not None: families[family.FId] = family
+            id = content[1:-1]
+            family = Family(id)
+        elif tag == 'NAME':
+            individual.IName = content
+        elif tag == 'SEX':
+            individual.IGen = content
+        elif tag == 'BIRT':
+            index += 1
+            line = lines[index].rstrip()
+            data = parse_line(line)
+            if data is None or not data[0] == 'DATE':
+                index += 1
+                continue
+            date = datetime.strptime(data[1], "%d %b %Y")
+            individual.IBirth = date
+        elif tag == 'DEAT':
+            index += 1
+            line = lines[index].rstrip()
+            data = parse_line(line)
+            if data is None or not data[0] == 'DATE':
+                index += 1
+                continue
+            date = datetime.strptime(data[1], "%d %b %Y")
+            individual.IDeath = date
+        elif tag == 'FAMC':
+            id = content[1:-1]
+            individual.IChild = id
+        elif tag == 'FAMS':
+            id = content[1:-1]
+            individual.ISpouse = id
+        elif tag == 'MARR':
+            index += 1
+            line = lines[index].rstrip()
+            data = parse_line(line)
+            if data is None or not data[0] == 'DATE':
+                index += 1
+                continue
+            date = datetime.strptime(data[1], "%d %b %Y")
+            family.FMar = date
+        elif tag == 'DIV':
+            index += 1
+            line = lines[index].rstrip()
+            data = parse_line(line)
+            if data is None or not data[0] == 'DATE':
+                index += 1
+                continue
+            date = datetime.strptime(data[1], "%d %b %Y")
+            family.FDiv = date
+        elif tag == 'HUSB':
+            id = content[1:-1]
+            family.FHusbId = id
+        elif tag == 'WIFE':
+            id = content[1:-1]
+            family.FWifeId = id
+        elif tag == 'CHIL':
+            id = content[1:-1]
+            family.FChildIds.append(id)
+
+        index += 1
+
+indivTable = PrettyTable()
+indivTable.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"]
+
+for key in sorted(individuals, key=lambda k: int(k[1:])):
+    indiv: Individual = individuals[key]
+    indivTable.add_row([indiv.IId, indiv.IName, indiv.IGen, indiv.IBirth.strftime("%Y-%m-%d"), indiv.get_age(), indiv.is_alive(), 
+                        indiv.IDeath if indiv.IDeath == 'NA' else indiv.IDeath.strftime("%Y-%m-%d"), indiv.IChild, indiv.ISpouse])
+print(indivTable)
+
+famTable = PrettyTable()
+famTable.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
+
+for key in sorted(families, key=lambda k: int(k[1:])):
+    fam: Family = families[key]
+    husb: Individual = individuals[fam.FHusbId]
+    wife: Individual = individuals[fam.FWifeId]
+    
+    famTable.add_row([fam.FId, fam.FMar.strftime("%Y-%m-%d"), fam.FDiv if fam.FDiv == 'NA' else fam.FDiv.strftime("%Y-%m-%d"), 
+                      fam.FHusbId, husb.IName, fam.FWifeId, wife.IName, fam.FChildIds])
+print(famTable)
